1c4f8c455389b25662da877196b8fe9ac49c61f5
Samir Lavingia <lavingiasa@localhost.(none)>
2012-12-29 16:45:59 -0800

    Changed the soundboard to a table view

---
+++ SoundBoard/SoundsInSoundBoardTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SoundsInSoundBoardTableViewController.h
+//  SoundBoard
+//
+//  Created by Samir Lavingia on 12/29/12.
+//  Copyright (c) 2012 SAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoreDataTableViewController.h"
+#import "SoundBoardGroup.h"
+
+@interface SoundsInSoundBoardTableViewController : CoreDataTableViewController
+
+@end
---
+++ SoundBoard/SoundsInSoundboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  SoundsInSoundboardViewController.h
+//  SoundBoard
+//
+//  Created by Samir Lavingia on 12/29/12.
+//  Copyright (c) 2012 SAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SoundsInSoundboardViewController : UITableViewController
+
+@end

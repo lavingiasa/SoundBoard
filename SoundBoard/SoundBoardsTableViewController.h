6be23d7603ea566d2f8789e8ec0bc4f172160901
Samir Lavingia <lavingiasa@localhost.(none)>
2012-12-29 16:09:22 -0800

    Added SoundboardTableViewController

---
+++ SoundBoard/SoundBoardsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  SoundBoardsTableViewController.h
+//  SoundBoard
+//
+//  Created by Samir Lavingia on 12/29/12.
+//  Copyright (c) 2012 SAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SoundBoardsTableViewController : UITableViewController
+
+@end

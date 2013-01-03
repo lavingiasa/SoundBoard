86fb429eedd11cd664391b3a79f2bf8412c73f59
Marcus Eng <Home@MacBook-Pro.local>
2013-01-03 15:19:56 -0800

    test

--- SoundBoard/SoundBoardSoundsViewController.m
+++ SoundBoard/SoundBoardSoundsViewController.m
@@ -31,7 +31,7 @@
     theAudio.delegate = self;
     [theAudio play];
     /*
-    For this method, we can add identifiers to the buttons (essentially numbers that get passed when a certain button is pressed), and these numbers that get passwed through can access an array with the sound in it...cutting down time and simplifying code.
+    For this method, we can add identifiers to the buttons (essentially numbers that get passed when a certain button is pressed), and these numbers that get passed through can access an array with the sound in it...cutting down time and simplifying code.
     */
 }
 

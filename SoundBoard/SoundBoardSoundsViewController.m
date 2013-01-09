683b528d1db05d21519d902d6934adb689c046f5
Samir Lavingia <lavingiasa@localhost.(none)>
2013-01-08 18:24:05 -0800

    deleted old code

--- SoundBoard/SoundBoardSoundsViewController.m
+++ SoundBoard/SoundBoardSoundsViewController.m
@@ -136,47 +136,6 @@
  
     }
 
-- (void) recordSound
-{
-    NSArray *dirPaths;
-    NSString *docsDir;
-    
-    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    docsDir = [dirPaths objectAtIndex:0];
-    NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"sound.caf"];
-    NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
-    NSDictionary *recordSettings = [NSDictionary
-                                    dictionaryWithObjectsAndKeys:
-                                    [NSNumber numberWithInt:AVAudioQualityMin],
-                                    AVEncoderAudioQualityKey,
-                                    [NSNumber numberWithInt:16],
-                                    AVEncoderBitRateKey,
-                                    [NSNumber numberWithInt: 2],
-                                    AVNumberOfChannelsKey,
-                                    [NSNumber numberWithFloat:44100.0],
-                                    AVSampleRateKey,
-                                    nil];
-    
-    NSError *error = nil;
-    
-    _audioRecorder = [[AVAudioRecorder alloc]
-                     initWithURL:soundFileURL
-                     settings:recordSettings
-                     error:&error];
-    
-    if (error)
-    {
-        NSLog(@"error: %@", [error localizedDescription]);
-        
-    } else {
-        [_audioRecorder prepareToRecord];
-    }
-    
-    [_audioRecorder recordForDuration:3.0];
-    
-}
-
-
 - (IBAction)addSound:(id)sender
 {
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
@@ -232,12 +191,6 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
     _imageFromCamera = image;
     [self dismissImagePicker];
     
-}
-
--(void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder
-                          successfully:(BOOL)flag
-{
-    NSLog(@"Done!");
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
